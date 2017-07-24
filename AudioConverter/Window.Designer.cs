c6af57f5e2d050a2af955765eb83d05baad19fc9
RkTeddy <838474413@qq.com>
2017-07-24 17:07:46 +0800

    Add buttons and labels

--- AudioConverter/Window.Designer.cs
+++ AudioConverter/Window.Designer.cs
@@ -5,7 +5,7 @@ using Arbingersys.Audio.Aumplib;
 
 namespace AudioConverter
 {
-    partial class Form1
+    partial class Window
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -33,12 +33,70 @@ namespace AudioConverter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sourceFileButton = new System.Windows.Forms.Button();
+            this.destFileButton = new System.Windows.Forms.Button();
+            this.sourceFileLabel = new System.Windows.Forms.Label();
+            this.destFileLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sourceFileButton
+            // 
+            this.sourceFileButton.Location = new System.Drawing.Point(12, 12);
+            this.sourceFileButton.Name = "sourceFileButton";
+            this.sourceFileButton.Size = new System.Drawing.Size(95, 35);
+            this.sourceFileButton.TabIndex = 0;
+            this.sourceFileButton.Text = "open";
+            this.sourceFileButton.UseVisualStyleBackColor = true;
+            // 
+            // destFileButton
+            // 
+            this.destFileButton.Location = new System.Drawing.Point(13, 106);
+            this.destFileButton.Name = "destFileButton";
+            this.destFileButton.Size = new System.Drawing.Size(94, 35);
+            this.destFileButton.TabIndex = 1;
+            this.destFileButton.Text = "save";
+            this.destFileButton.UseVisualStyleBackColor = true;
+            // 
+            // sourceFileLabel
+            // 
+            this.sourceFileLabel.AutoSize = true;
+            this.sourceFileLabel.Location = new System.Drawing.Point(188, 17);
+            this.sourceFileLabel.Name = "sourceFileLabel";
+            this.sourceFileLabel.Size = new System.Drawing.Size(111, 25);
+            this.sourceFileLabel.TabIndex = 2;
+            this.sourceFileLabel.Text = "Not selected";
+            // 
+            // destFileLabel
+            // 
+            this.destFileLabel.AutoSize = true;
+            this.destFileLabel.Location = new System.Drawing.Point(188, 106);
+            this.destFileLabel.Name = "destFileLabel";
+            this.destFileLabel.Size = new System.Drawing.Size(111, 25);
+            this.destFileLabel.TabIndex = 3;
+            this.destFileLabel.Text = "Not selected";
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 321);
+            this.Controls.Add(this.destFileLabel);
+            this.Controls.Add(this.sourceFileLabel);
+            this.Controls.Add(this.destFileButton);
+            this.Controls.Add(this.sourceFileButton);
+            this.Name = "Window";
+            this.Text = "Audio Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button sourceFileButton;
+        private Button destFileButton;
+        private Label sourceFileLabel;
+        private Label destFileLabel;
     }
 }
 
